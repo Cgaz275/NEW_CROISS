8f32a933ec8db16986e4567720e9dfce739c1593
anthonysimonIsCoding <duonglekhanh345@gmail.com>
2023-12-05 19:07:13 +0700

    CROISS TAI SINH

--- ProductPage/productpage.js
+++ ProductPage/productpage.js
@@ -78,6 +78,7 @@ let goToPreviousPage = () => {
     currentPage--;
     showPage();
     updatePaginationButtons();
+
   }
 };
 
@@ -88,6 +89,7 @@ let goToNextPage = () => {
     currentPage++;
     showPage();
     updatePaginationButtons();
+
   }
 };
 
@@ -102,6 +104,7 @@ let filterByCategory = (category) => {
 };
 
 let increment = (id) => {
+    updatePaginationButtons();
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
 
@@ -120,6 +123,7 @@ let increment = (id) => {
   localStorage.setItem('cart', JSON.stringify(basket));
 };
 let decrement = (id) => {
+updatePaginationButtons();
   let selectedItem = id;
   let search = basket.find((x) => x.id === selectedItem.id);
 
