fcbefbe961b024452c1d2ef0dbfdbf25c79ba254
Cgaz275 <alanewiston2@gmail.com>
2023-12-05 23:32:14 +0700

    c

--- ProductPage/productpage.js
+++ ProductPage/productpage.js
@@ -185,15 +185,44 @@ function generateUniqueId() {
 }
 generateShop();
 
-function checkLogIn(){
+// function checkLogIn(){
 
-    let currentUser = localStorage.getItem("currentUser");
-    console.log(currentUser);
-    if(currentUser){
-       location.assign("../Cart/cart.html");
-    }else{
-       location.assign("../Login/sign-in.html");
-    }
-}
+//     let currentUser = localStorage.getItem("currentUser");
+//     console.log(currentUser);
+//     if(currentUser){
+//        location.assign("../Cart/cart.html");
+//     }else{
+//        location.assign("../Login/sign-in.html");
+//     }
+// }
 
 showPage();
+
+/**------------------------------------- */
+
+
+function checkLogIn(){
+
+  let currentUser = localStorage.getItem("currentUser");
+  console.log(currentUser);
+  if(currentUser){
+     location.assign("User/User.html");
+  }else{
+     location.assign("Login/sign-in.html");
+  }
+}
+function checkLogInC(){
+
+ let currentUser = localStorage.getItem("currentUser");
+ console.log(currentUser);
+ if(currentUser){
+    location.assign("/Cart/cart.html");
+ }else{
+    location.assign("/Login/sign-in.html");
+ }
+}
+
+function logOut(){
+ localStorage.removeItem("currentUser");
+ location.assign("/Login/sign-in.html");
+}
