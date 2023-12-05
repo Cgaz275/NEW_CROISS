4cc56144aa233cd7585ec737c11ff902ff9cc6a9
Huy Trang <chinchindethw@gmail.com>
2023-12-05 16:19:40 +0700

    update nè

--- User/OrderHistory.js
+++ User/OrderHistory.js
@@ -66,16 +66,17 @@ let generateShop = () => {
                  let orderTimestamp = orders.length > 0 ? orders[0].timestamp || '' : '';
 
                 return `
-                  <div class="details">
-                    <h3>Bill ID: ${orderID}</h3>
-                    <h4>User Name: ${uniqueUsername}</h4>
-                    <p>Address: ${uniqueAddress}</p>
-                    <p>Payment Method: ${uniquePaymentMethod}</p>
-                    <p>Shipped: ${uniqueShippingStatus ? "Yes" : "No"}</p>
-                    <p>Order Timestamp: ${orderTimestamp}</p>
-                    ${orderHTML}
-
-                  </div>
+                      <div class="details">
+                          <div class = "history">
+                            <span>Bill ID:${orderID}<span> 
+                            <span>User Name: ${uniqueUsername}</span>
+                            <span>Address: ${uniqueAddress}</span>
+                            <span>Payment Method: ${uniquePaymentMethod}</span>
+                            <span>Shipped: ${uniqueShippingStatus ? "Yes" : "No"}</span>
+                            <span>Order Timestamp: ${orderTimestamp}</span>
+                            ${orderHTML}
+                          </div>
+                      </div>
                 `;
               })
               .join("")
--- User/User.js
+++ User/User.js
@@ -29,8 +29,8 @@ if (currentUserData) {
             <p>User Information</p>
             <span>Username:</span> <input type="text" value="${username}" readonly>           
             <span>Name:</span> <input type="text" id="nameInput" value="${name}">
-            <span>Address:</span> <input type="text" value="${address}">
-            <span>Phone number:</span> <input type="number" value="${phone}">
+            <span>Address:</span> <input type="text" id="addressInput" value="${address}">
+            <span>Phone number:</span> <input type="number" value="${phone}" readonly>
             <button>Save changes</button>
           </div>
         </div>
@@ -43,3 +43,23 @@ if (currentUserData) {
 } else {
   console.log("User not found");
 }
+
+let nameInput = document.getElementById("nameInput");
+
+nameInput.addEventListener("input", (event) => {
+  // Lắng nghe sự kiện khi người dùng nhập vào ô tên
+  currentUserData.name = event.target.value;
+  // Lưu lại dữ liệu đã thay đổi vào localStorage
+  localStorage.setItem("shopUsersData", JSON.stringify(newshopUsersData));
+});
+
+let addressInput = document.getElementById("addressInput");
+
+addressInput.addEventListener("input", (event) => {
+  // Lắng nghe sự kiện khi người dùng nhập vào ô địa chỉ
+  currentUserData.address = event.target.value;
+  // Lưu lại dữ liệu đã thay đổi vào localStorage
+  localStorage.setItem("shopUsersData", JSON.stringify(newshopUsersData));
+});
+
+
